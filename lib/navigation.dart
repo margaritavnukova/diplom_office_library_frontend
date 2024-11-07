38c68ca24148c653d4274d950f419a844a3ed975
margaritavnukova <margaritavnukova82@gmail.com>
2024-11-07 15:45:33 +0300

    lesson 6.1 navigation (part 1) added Page2 to MainPage & Page1

--- lib/navigation.dart
+++ lib/navigation.dart
@@ -23,11 +23,24 @@ class MainPage extends StatelessWidget{
                       await Navigator.of(context).push(
                         MaterialPageRoute(
                           builder: (context) => 
-                          const Page1(title: "Page 1"),
+                          const Page1(title: "blue"),
                         )
                       );
                     }, 
                     child: const Text("To page 1")
+                  ),
+
+                  ElevatedButton(
+                    onPressed: () async {
+                      final String? result = 
+                      await Navigator.of(context).push(
+                        MaterialPageRoute(
+                          builder: (context) => 
+                          const Page2(title: "green"),
+                        )
+                      );
+                    }, 
+                    child: const Text("To page 2")
                   )
                 ],
               ),
@@ -55,6 +68,36 @@ class Page1 extends StatelessWidget{
         child: Column(
           children: [
             Text("Page 1 - $title"),
+            ElevatedButton(
+              onPressed: () async {
+                Navigator.of(context).pop("resulting");
+              },
+              child: const Text("Return"),
+            )
+          ],
+        )
+      )
+    );
+  }
+}
+
+
+class Page2 extends StatelessWidget{
+
+  const Page2({super.key, required this.title});
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.green,
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Text("Page 2 - $title"),
             ElevatedButton(
               onPressed: () async {
                 Navigator.of(context).pop("resulting");
